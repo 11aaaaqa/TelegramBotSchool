ff904bab04183bfa5abaf10e879f68586b776822
aaaaqa <Neg@DESKTOP-BR68L5I>
2024-02-08 21:21:16 +0400

    добавил возможность отображения напоминаний

--- TelegramBotSchool/Commands/ShowAllCommand.cs
+++ TelegramBotSchool/Commands/ShowAllCommand.cs
@@ -1,4 +1,5 @@
-﻿using Telegram.Bot;
+﻿using System.Text;
+using Telegram.Bot;
 using Telegram.Bot.Types;
 using TelegramBotSchool.Database;
 using TelegramBotSchool.Services;
@@ -19,7 +20,29 @@ namespace TelegramBotSchool.Commands
         public override string Name => "showAll";
         public override async Task ExecuteAsync(Update update)
         {
-            var list = context.Reminders.ToList();
+            var toBackMarkup = markup.GetToBack();
+            var message = update.CallbackQuery.Message;
+
+            if (context.Reminders.Any(x => x.ChatId == message.Chat.Id))
+            {
+                var reminders = context.Reminders.Where(x => x.ChatId == message.Chat.Id).OrderBy(x => x.ReminderTime);
+
+                StringBuilder sb = new StringBuilder();
+                int counter = 1;
+
+                foreach (var reminder in reminders)
+                {
+                    sb.Append(counter + ") " + reminder.TextOfReminder + "\n" + reminder.ReminderTime + "\n");
+                    counter++;
+                }
+
+                await client.EditMessageTextAsync(message.Chat.Id, message.MessageId, "Вот все, о чем мне надо напомнить \n" + $"{sb}",
+                    replyMarkup: toBackMarkup);
+            }
+            else
+            {
+                await client.EditMessageTextAsync(message.Chat.Id, message.MessageId, "У вас нет напоминаний", replyMarkup: toBackMarkup);
+            }
         }
     }
 }
