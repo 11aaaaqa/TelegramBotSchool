35eec35e0b11689d6076b137c3af1cae32dbed98
aaaaqa <Neg@DESKTOP-BR68L5I>
2024-02-10 19:30:57 +0400

    создал новые миграции

--- TelegramBotSchool/Migrations/20240210150709_Initt.Designer.cs
+++ TelegramBotSchool/Migrations/20240210150709_Initt.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TelegramBotSchool.Database;
 
@@ -11,9 +12,11 @@ using TelegramBotSchool.Database;
 namespace TelegramBotSchool.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240210150709_Initt")]
+    partial class Initt
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -255,7 +258,7 @@ namespace TelegramBotSchool.Migrations
                     b.Property<short>("Difference")
                         .HasColumnType("smallint");
 
-                    b.Property<bool>("IsDeleteReminder")
+                    b.Property<bool>("IsDelete")
                         .HasColumnType("bit");
 
                     b.Property<bool>("IsDifferenceSet")
---
+++ TelegramBotSchool/Migrations/20240210151030_Inittt.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TelegramBotSchool.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inittt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsDelete",
+                table: "Users",
+                newName: "IsDeleteReminder");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsDeleteReminder",
+                table: "Users",
+                newName: "IsDelete");
+        }
+    }
+}
