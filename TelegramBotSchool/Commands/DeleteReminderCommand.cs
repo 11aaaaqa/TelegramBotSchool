d65ce98bb905bb480e067eb1627a013aaacaf3c8
aaaaqa <Neg@DESKTOP-BR68L5I>
2024-02-10 19:37:36 +0400

    добавил комманду, вызываемую при нажатии на кнопку "удалить"

---
+++ TelegramBotSchool/Commands/DeleteReminderCommand.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Text;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using TelegramBotSchool.Database;
+using TelegramBotSchool.Services;
+
+namespace TelegramBotSchool.Commands
+{
+    public class DeleteReminderCommand : BaseCommand
+    {
+        private readonly TelegramBotClient client;
+        private readonly IGetInlineKeyboardMarkup markup;
+        private readonly ApplicationDbContext context;
+        public DeleteReminderCommand(Bot bot, IGetInlineKeyboardMarkup markup, ApplicationDbContext context)
+        {
+            client = bot.GetBot();
+            this.markup = markup;
+            this.context = context;
+        }
+        public override string Name => "delete";
+        public override async Task ExecuteAsync(Update update)
+        {
+            var toBackMarkup = markup.GetToBack();
+            var message = update.CallbackQuery.Message;
+            var reminders = await context.Reminders.Where(x => x.ChatId == message.Chat.Id).OrderBy(x => x.ReminderId).ToListAsync();
+
+            if (reminders.Count == 0)
+            {
+                await client.EditMessageTextAsync(message.Chat.Id, message.MessageId, "У вас нет напоминаний",
+                    replyMarkup:toBackMarkup);
+                return;
+            }
+
+            StringBuilder sb = new StringBuilder();
+            
+            foreach (var reminder in reminders)
+            {
+                sb.Append(reminder.ReminderId + ") " + reminder.TextOfReminder + "\n" + reminder.ReminderTime + "\n");
+            }
+
+            var user = await context.Users.SingleOrDefaultAsync(x => x.ChatId == message.Chat.Id.ToString());
+            user.IsDeleteReminder = true;
+
+            await context.SaveChangesAsync();
+            await client.EditMessageTextAsync(message.Chat.Id, message.MessageId, "Введите номер напоминания для удаления \n" + $"{sb}",
+                replyMarkup:toBackMarkup);
+        }
+    }
+}
