be8c51e8dc0deaa84a26f0926889b46b5084f42e
aaaaqa <Neg@DESKTOP-BR68L5I>
2024-02-24 14:29:30 +0400

    добавил модель запланированного расписания

---
+++ TelegramBotSchool/Models/ScheduledReminder.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TelegramBotSchool.Models
+{
+    public class ScheduledReminder
+    {
+        [Key]
+        public Guid ReminderId { get; set; }
+        public string JobId { get; set; }
+    }
+}
