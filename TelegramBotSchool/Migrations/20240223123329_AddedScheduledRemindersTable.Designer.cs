1cf031f67b0352f93d35f548814e41d02427c261
aaaaqa <Neg@DESKTOP-BR68L5I>
2024-02-24 14:43:47 +0400

    добавил миграцию с таблицей запланированных напоминаний

--- TelegramBotSchool/Migrations/20240223123329_AddedScheduledRemindersTable.Designer.cs
+++ TelegramBotSchool/Migrations/20240223123329_AddedScheduledRemindersTable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TelegramBotSchool.Database;
 
@@ -11,9 +12,11 @@ using TelegramBotSchool.Database;
 namespace TelegramBotSchool.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240223123329_AddedScheduledRemindersTable")]
+    partial class AddedScheduledRemindersTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ TelegramBotSchool/Migrations/20240223123329_AddedScheduledRemindersTable.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TelegramBotSchool.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedScheduledRemindersTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ScheduledReminders",
+                columns: table => new
+                {
+                    ReminderId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    JobId = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ScheduledReminders", x => x.ReminderId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ScheduledReminders");
+        }
+    }
+}
